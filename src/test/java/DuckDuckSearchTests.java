c9904f37731d23d04f750d24e5ed3ec72caf5c4e
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2026-02-03 18:14:52 +0300

    Add DuckDuck simple search test

---
+++ src/test/java/DuckDuckSearchTests.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DuckDuckSearchTests {
+
+    @Test
+    void duckDuckQaGuruSearchTest() {
+        open("https://duckduckgo.com/");
+        $("#searchbox_input").setValue("QA Guru").pressEnter();
+        $("#web_content_wrapper").shouldHave(text("qa.guru"));
+    }
+}
