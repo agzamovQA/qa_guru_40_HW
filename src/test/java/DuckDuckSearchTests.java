56af342b8f901ba8eea602268e52a7038cd19a14
Agzamov Vladimir <agzamov.qa@yandex.ru>
2026-02-04 17:43:06 +0300

    test

--- src/test/java/DuckDuckSearchTests.java
+++ src/test/java/DuckDuckSearchTests.java
@@ -11,4 +11,11 @@ public class DuckDuckSearchTests {
         $("#searchbox_input").setValue("QA Guru").pressEnter();
         $("#web_content_wrapper").shouldHave(text("qa.guru"));
     }
+
+    @Test
+    void duckDuckQaGuruSearchTest() {
+        open("https://duckduckgo.com/");
+        $("#searchbox_input").setValue("QA Guru").pressEnter();
+        $("#web_content_wrapper").shouldHave(text("qa.guru"));
+    }
 }
