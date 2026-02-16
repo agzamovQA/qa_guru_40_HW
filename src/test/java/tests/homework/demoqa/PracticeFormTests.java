208fb8a26a468d751346c72c3a6e224ab83c0839
Agzamov Vladimir <agzamov.qa@yandex.ru>
2026-02-16 17:55:46 +0300

    Вынес переменные в отдельный класс

--- src/test/java/tests/homework/demoqa/PracticeFormTests.java
+++ src/test/java/tests/homework/demoqa/PracticeFormTests.java
@@ -2,6 +2,7 @@ package tests.homework.demoqa;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import tests.testdata.TestData;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -10,54 +11,40 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests extends TestBase {
 
-    String firstName = "Jhony";
-    String lastName = "Silverhand";
-    String userEmail = "samurai2023@nc.com";
-    String userGender = "Male";
-    String userNumber = "0202320770";
-    String yearOfBirth = "2000";
-    String monthOfBirth = "November";
-    String dateOfBirth = "23";
-    String userSubjectChoice = "English";
-    String userHobbies = "Music";
-    String userHomeAdress = "Night City, District Kabuki, Home 20, apart 77";
-    String userState = "NCR";
-    String userCity = "Noida";
-
     @Test
     @DisplayName("[Positive] Fill all fields and check table result")
     void successfulFillRegistrationFormTest() {
         open("/automation-practice-form");
-
         executeJavaScript("$('footer').remove();");
         executeJavaScript("$('#fixedban').remove();");
 
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(userEmail);
-        $("#genterWrapper").$(byText(userGender)).click();
-        $("#userNumber").setValue(userNumber);
+
+        $("#firstName").setValue(TestData.firstName);
+        $("#lastName").setValue(TestData.lastName);
+        $("#userEmail").setValue(TestData.userEmail);
+        $("#genterWrapper").$(byText(TestData.userGender)).click();
+        $("#userNumber").setValue(TestData.userNumber);
 
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").$(byText(yearOfBirth)).click();
-        $(".react-datepicker__month-select").$(byText(monthOfBirth)).click();
-        $(".react-datepicker__month").$(byText(dateOfBirth)).click();
+        $(".react-datepicker__year-select").$(byText(TestData.yearOfBirth)).click();
+        $(".react-datepicker__month-select").$(byText(TestData.monthOfBirth)).click();
+        $(".react-datepicker__month").$(byText(TestData.dateOfBirth)).click();
 
         //Выбор предмета с обычном вводом текста
-        $("#subjectsInput").setValue(userSubjectChoice).pressEnter();
+        $("#subjectsInput").setValue(TestData.userSubjectChoice).pressEnter();
 
 //        Выбор предмета с вводом первой буквы и выбором совпадения из меню с автокомплитом
 //        $("#subjectsInput").setValue("E");
 //        $(".subjects-auto-complete__menu").$(byText(userSubjectChoice)).click();
 
-        $("#hobbiesWrapper").$(byText(userHobbies)).click();
-        $("#currentAddress").setValue(userHomeAdress);
+        $("#hobbiesWrapper").$(byText(TestData.userHobbies)).click();
+        $("#currentAddress").setValue(TestData.userHomeAdress);
         $("#uploadPicture").uploadFromClasspath("Jhonny_Silverhand.jpg");
 
         $("#state").click();
-        $("#state").$(byText(userState)).click();
+        $("#state").$(byText(TestData.userState)).click();
         $("#city").click();
-        $("#city").$(byText(userCity)).click();
+        $("#city").$(byText(TestData.userCity)).click();
 
         $("#submit").click();
 
@@ -65,16 +52,16 @@ public class PracticeFormTests extends TestBase {
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
 
         //Проверяем соответствие ключ-значение
-        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text(firstName + " " + lastName));
-        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text(userEmail));
-        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text(userGender));
-        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text(userNumber));
-        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text(dateOfBirth + " " + monthOfBirth + "," + yearOfBirth));
-        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text(userSubjectChoice));
-        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text(userHobbies));
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text(TestData.firstName + " " + TestData.lastName));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text(TestData.userEmail));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text(TestData.userGender));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text(TestData.userNumber));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text(TestData.dateOfBirth + " " + TestData.monthOfBirth + "," + TestData.yearOfBirth));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text(TestData.userSubjectChoice));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text(TestData.userHobbies));
         $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("Jhonny_Silverhand.jpg"));
-        $(".table-responsive").$(byText("Address")).parent().shouldHave(text(userHomeAdress));
-        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text(userState + " " + userCity));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text(TestData.userHomeAdress));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text(TestData.userState + " " + TestData.userCity));
 
     }
 
@@ -87,19 +74,19 @@ public class PracticeFormTests extends TestBase {
         executeJavaScript("$('footer').remove();");
         executeJavaScript("$('#fixedban').remove();");
 
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("#genterWrapper").$(byText(userGender)).click();
-        $("#userNumber").setValue(userNumber);
+        $("#firstName").setValue(TestData.firstName);
+        $("#lastName").setValue(TestData.lastName);
+        $("#genterWrapper").$(byText(TestData.userGender)).click();
+        $("#userNumber").setValue(TestData.userNumber);
 
         $("#submit").click();
 
         //Проверяем ответ в таблице
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
 
-        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text(firstName + " " + lastName));
-        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text(userGender));
-        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text(userNumber));
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text(TestData.firstName + " " + TestData.lastName));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text(TestData.userGender));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text(TestData.userNumber));
 
     }
 
@@ -112,10 +99,10 @@ public class PracticeFormTests extends TestBase {
         executeJavaScript("$('footer').remove();");
         executeJavaScript("$('#fixedban').remove();");
 
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("#genterWrapper").$(byText(userGender)).click();
-        $("#userNumber").setValue(userNumber);
+        $("#firstName").setValue(TestData.firstName);
+        $("#lastName").setValue(TestData.lastName);
+        $("#genterWrapper").$(byText(TestData.userGender)).click();
+        $("#userNumber").setValue(TestData.userNumber);
         $("#userEmail").setValue("samurai2023nc.com");
 
         $("#submit").click();
---
+++ src/test/java/tests/testdata/TestData.java
@@ -0,0 +1,17 @@
+package tests.testdata;
+
+public class TestData {
+    public static String firstName = "Jhony",
+            lastName = "Silverhand",
+            userEmail = "samurai2023@nc.com",
+            userGender = "Male",
+            userNumber = "0202320770",
+            yearOfBirth = "2000",
+            monthOfBirth = "November",
+            dateOfBirth = "23",
+            userSubjectChoice = "English",
+            userHobbies = "Music",
+            userHomeAdress = "Night City, District Kabuki, Home 20, apart 77",
+            userState = "NCR",
+            userCity = "Noida";
+}
