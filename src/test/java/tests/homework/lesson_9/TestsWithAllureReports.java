cf63b5487a977ce5e9c2bd9061d651b3c2644ecb
Agzamov Vladimir <agzamov.qa@yandex.ru>
2026-03-19 07:39:15 +0300

    9HW

---
+++ src/test/java/tests/homework/lesson_9/TestsWithAllureReports.java
@@ -0,0 +1,77 @@
+package tests.homework.lesson_9;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+
+public class TestsWithAllureReports {
+
+    private static final String REPOSITORY = "agzamovQA/qa_guru_40_HW";
+    private static final int ISSUE = 1;
+
+    @Test
+    @DisplayName("Чистый Selenide")
+    public void simpleIssueTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("https://github.com");
+
+        $(".input-button").click();
+        $("#query-builder-test").sendKeys(REPOSITORY);
+        $("#query-builder-test").submit();
+
+        $(linkText(REPOSITORY)).click();
+        $("[data-content='Issues']").click();
+        $(withText("#" + ISSUE)).should(Condition.exist);
+    }
+
+    @Test
+    @DisplayName("Лямбда шаги через step")
+    public void testLambdaStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+        });
+
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $(".input-button").click();
+            $("#query-builder-test").sendKeys(REPOSITORY);
+            $("#query-builder-test").submit();
+        });
+
+        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+
+        step("Открываем таб Issues", () -> {
+            $("[data-content='Issues']").click();
+        });
+
+        step("Проверяем наличие Issue с номером " + ISSUE, () -> {
+            $(withText("#" + ISSUE)).should(Condition.exist);
+        });
+    }
+
+    @Test
+    @DisplayName("Шаги с аннотацией @Step")
+    public void testAnnotatedStep() {
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssueTab();
+        steps.shouldSeeIssueWithNumber(ISSUE);
+        steps.takeScreenshot();
+
+    }
+}
