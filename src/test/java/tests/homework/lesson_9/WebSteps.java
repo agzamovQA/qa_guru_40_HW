5b9a2a7f2a3b7ad7b2ecc2b3a3f3730d1a17f10b
Agzamov Vladimir <agzamov.qa@yandex.ru>
2026-03-24 16:53:28 +0300

    Удалил ссылку в степах

--- src/test/java/tests/homework/lesson_9/WebSteps.java
+++ src/test/java/tests/homework/lesson_9/WebSteps.java
@@ -17,7 +17,7 @@ public class WebSteps {
 
     @Step ("Открываем главную страницу")
     public void openMainPage() {
-        open("https://github.com");
+        open("");
     }
 
     @Step ("Ищем репозиторий {repo}")
